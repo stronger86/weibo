d0d25c4fce976c5202cf703bca8d1f081b759e63
pengzhizhuang <pengzhizhuang@100tal.com>
2018-12-23 21:44:22 +0800

    _status page

--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -1,9 +1,9 @@
 <li class="media mt-4 mb-4">
-    <a href="{{ route('users.show', $user->id )}}">
-        <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="mr-3 gravatar"/>
+    <a href="{{ route('users.show', $status->user->id )}}">
+        <img src="{{ $status->user->gravatar() }}" alt="{{ $status->user->name }}" class="mr-3 gravatar"/>
     </a>
     <div class="media-body">
-        <h5 class="mt-0 mb-1">{{ $user->name }} <small> / {{ $status->created_at->diffForHumans() }}</small></h5>
+        <h5 class="mt-0 mb-1">{{ $status->user->name }} <small> / {{ $status->created_at->diffForHumans() }}</small></h5>
         {{ $status->content }}
     </div>
 
